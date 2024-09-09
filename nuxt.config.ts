d2d5ee5ba320fd94d81ec4dc5a45cef8cd247bf6
Roman Marunkevich <vinnibea@gmail.com>
2024-09-09 23:26:25 +0300

    try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   runtimeConfig: {
     mongoUrl: process.env.MONGO_DB_URI,
     bot: process.env.BOT,
   },
-})
+
+  nitro: {
+    plugins: ["~server/plugins/index.js", "~server/plugins/bot.js"],
+  },
+});
