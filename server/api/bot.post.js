618970215118df28c288101882f364abd3290fa0
Roman Marunkevich <vinnibea@gmail.com>
2025-10-07 17:49:16 +0300

    post bot

---
+++ server/api/bot.post.js
@@ -0,0 +1,14 @@
+import  { bot } from '../bot_main';
+
+export default defineEventHandler(async (event) => {
+   
+    try {
+        const body = await readBody(event);
+        await bot.handleUpdate(body);
+        return { status: 'ok' };
+    } catch (error) {
+        throw createError({
+            status: 'Ну таке..'
+        })
+    }
+})
