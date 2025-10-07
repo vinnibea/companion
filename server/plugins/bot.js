91f902bc6353306f8c661b46cf04a53f26a7bdd9
Roman Marunkevich <vinnibea@gmail.com>
2025-10-07 21:33:35 +0300

    dev

--- server/plugins/bot.js
+++ server/plugins/bot.js
@@ -6,7 +6,7 @@ export default defineNitroPlugin(async (app) => {
    
     if (process.env.NODE_ENV === 'development') {
         console.log('Development mode, launching bot without webhook');
-        bot.launch();
+        // bot.launch();
     } else {
         bot.telegram.setWebhook('https://companion-kohl.vercel.app/api/bot')
     }
